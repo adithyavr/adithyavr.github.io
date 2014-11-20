7cd6a8edbfe4554ebddcf1aedb8ad9753abb5268
adithyavr <adithya.we.r@gmail.com>
2014-11-21 00:28:32 +0530

    first commit

---
+++ universities_points.js
@@ -0,0 +1,130 @@
+var point_best_universities={
+ "type":"FeatureCollection",
+ 
+ "features":
+ [
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-86.5153998026856,39.17720475]
+	},
+ "properties": {"popupContent":"Indiana University"}
+},
+
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-117.84016483553,33.6431901]
+	},
+ "properties": {"popupContent":"University Of California, Irvine"}
+},
+
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-86.923893579845,40.4319722]
+	},
+ "properties": {"popupContent":"Purdue University"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-105.266073019244,40.009291]
+	},
+ "properties": {"popupContent":"University of Colorado, Boulder"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-84.3988407100158,33.776033]
+	},
+ "properties": {"popupContent":"Georgia Tech university"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-78.4086255,40.5400689	]
+	},
+ "properties": {"popupContent":"Pen state University"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-76.9423044,38.9856989]
+	},
+ "properties": {"popupContent":"University of Maryland CP"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-88.2313495137072,40.1006938]
+	},
+ "properties": {"popupContent":"University of Illionois - Urbana Campaign"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-83.7100148368311,42.2942077]
+	},
+ "properties": {"popupContent":"University of Michigan - Ann Arbor"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-93.235353,44.974747]
+	},
+ "properties": {"popupContent":"University of Minnesota - Twin Cities"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-122.308200692377,47.6543421]
+	},
+ "properties": {"popupContent":"University of Washington"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-78.5035322,38.0353682]
+	},
+ "properties": {"popupContent":"University of Virginia"}
+},
+{
+  "type":"Feature",
+  "bbox":["33.6315566","33.654776","-117.8595296","-117.8219357"],
+  "geometry":
+	{"type":"Point",
+	 "coordinates":[-76.9404203,38.9530767]
+	},
+ "properties": {"popupContent":"University of Maryland, BC"}
+}
+]
+
+}
+
+
+
